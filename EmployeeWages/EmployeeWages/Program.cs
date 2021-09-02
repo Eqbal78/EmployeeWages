265e79aebe3093b0ecb3227ea412c70192caf97d
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 10:38:16 +0530

    Calculate Employee Wages on Conditions

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -9,15 +9,19 @@ namespace EmployeeWages
             int Full_Time = 1;
             int Part_Time = 2;
             int Emp_rate_per_Hrs = 20;
-            int No_Of_Working_Days = 20;
-            int empHrs, empWage;
+            int Max_Working_Days = 20;
+            int Max_Working_Hrs = 100;
+            int empHrs = 0, empWage=0;
+            int workingHrs = 0;
             int totalEmpWage = 0;
+            int No_of_Working_Days = 0;
 
-            for (int i = 0; i < No_Of_Working_Days; i++)
+
+            while (No_of_Working_Days <= Max_Working_Days && workingHrs <= Max_Working_Hrs)
             {
+                No_of_Working_Days++;
                 Random r = new Random();
                 int check = r.Next(1, 4);
-
                 switch (check)
                 {
                     case 1:
@@ -30,13 +34,12 @@ namespace EmployeeWages
                         empHrs = 0;
                         break;
                 }
-
+                workingHrs += empHrs;
+                Console.WriteLine("Days: " + workingHrs + " Emp Hrs: " + empHrs);
                 empWage = empHrs * Emp_rate_per_Hrs;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wages: " + empWage);
             }
-
-            Console.WriteLine("Total Emp Wages: " + totalEmpWage);
+            //empWage = empHrs * Emp_rate_per_Hrs;
+            Console.WriteLine("Employee Wages: " + empWage);
         }
     }
 }
