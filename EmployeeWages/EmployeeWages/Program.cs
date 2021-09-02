fe35474045ef049e5f6e21c8ee8951ea3e52b553
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 10:29:21 +0530

    Calculate Employee Month Wages

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -9,25 +9,34 @@ namespace EmployeeWages
             int Full_Time = 1;
             int Part_Time = 2;
             int Emp_rate_per_Hrs = 20;
+            int No_Of_Working_Days = 20;
             int empHrs, empWage;
-            Random r = new Random();
-            int check = r.Next(1, 4);
+            int totalEmpWage = 0;
 
-            switch (check)
+            for (int i = 0; i < No_Of_Working_Days; i++)
             {
-                case 1:
-                    empHrs = 8;
-                    break;
-                case 2:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random r = new Random();
+                int check = r.Next(1, 4);
+
+                switch (check)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = empHrs * Emp_rate_per_Hrs;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Wages: " + empWage);
             }
 
-            empWage = empHrs * Emp_rate_per_Hrs;
-            Console.WriteLine("Employee Wages: " + empWage);
+            Console.WriteLine("Total Emp Wages: " + totalEmpWage);
         }
     }
 }
