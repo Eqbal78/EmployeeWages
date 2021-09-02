53a7d6344ca1ffd94b37acee1c28e0b7af76ff9a
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 10:05:15 +0530

    Check Employee Attendance

---
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            Console.WriteLine("Welcome to Employee Wage Computation Program using C#");
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyoee is Absent");
+            }
+        }
+    }
+}
