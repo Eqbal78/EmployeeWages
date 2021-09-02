5138650bed99d5c98ea4bbdfdc30f9aed6c017e0
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 10:19:57 +0530

    Add Part Time Employee

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -7,13 +7,18 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             int Full_Time = 1;
+            int Part_Time = 2;
             int Emp_rate_per_Hrs = 20;
             int empHrs, empWage;
             Random r = new Random();
-            int check = r.Next(1, 3);
+            int check = r.Next(1, 4);
             if (check == Full_Time)
             {
                 empHrs = 8;
+            }
+            else if (check == Part_Time)
+            {
+                empHrs = 4;
             }
             else
                 empHrs = 0;
